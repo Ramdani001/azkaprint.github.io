3dd6c75f2c62a77caa2d5b9d86d91ff486bf8780
Ramdani <rizkan0990@gmail.com>
2022-07-01 07:32:06 +0700

    Add Animate & Contact Us

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
-     
+     animation: {
+      'jump': 'jump 2s ease-in-out infinite',
+    },
+    keyframes: {
+      jump: {
+        '0%, 100%': { transform: 'translateY(-5%); '},
+        '50%':  { transform: 'translateY(0);'},
+
+      }
+    } ,
   },
   plugins: [],
 }
